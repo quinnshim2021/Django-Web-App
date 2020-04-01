0897b8615c8c40ba657210923f16e9a3acb21b81
quinnrs98 <quinnrs98@gmail.com>
2020-04-01 17:02:11 -0400

    aws

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['quinnfirstdjangoapp.herokuapp.com'] # put website url here
 # Application definition
 
 INSTALLED_APPS = [
+    'storages',
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
     'crispy_forms',
@@ -148,3 +149,10 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 django_heroku.settings(locals())
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
--- users/models.py
+++ users/models.py
@@ -8,15 +8,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
-
-    # override save to add our own funcitonality (resize large images)
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-
-        # all 300's in this check can be changed to any size, 300 is our set max size
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
