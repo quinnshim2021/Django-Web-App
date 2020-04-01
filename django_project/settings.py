c50317988db1f6b28b650b344ea6d8e0605707e8
quinnrs98 <quinnrs98@gmail.com>
2020-04-01 02:34:10 -0400

    trying to still figure out debug

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = 'fingdinz4!kvke^e@zpg6^dx8a4l#8k2jej2ahz+j273-+)0h8'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 ALLOWED_HOSTS = ['quinnfirstdjangoapp.herokuapp.com']
 
